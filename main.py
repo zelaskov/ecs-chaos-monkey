839f18a9d0bfdac201529a31f2048af9320a3d6d
zelaskov <marcin.zelasko@codetain.eu>
2021-10-27 22:13:26 +0200

    general: fix some linting

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ from dotenv import load_dotenv
 load_dotenv()
 cluster=os.environ.get("CLUSTER_NAME")
 region=os.environ.get("REGION")
-ecs = boto3.client('ecs', region_name=region)
+ecs = boto3.client("ecs", region_name=region)
 
 def lambda_handler():
     response = ecs.list_services(
         cluster=cluster,
         )
-    services = response['serviceArns']
+    services = response["serviceArns"]
     return services
 
 def describe_services():
