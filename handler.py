8d4da1e6a333ea267510e60268137279e3931f2d
zelaskov <marcin.zelasko@codetain.eu>
2021-10-28 12:57:38 +0200

    cleanup lambda

--- handler.py
+++ handler.py
@@ -3,7 +3,7 @@ import random
 import json
 
 
-cluster="cluster_name"
+cluster="some_cluster"
 region="some_region"
 ecs = boto3.client("ecs", region_name=region)
 
@@ -15,12 +15,9 @@ def list_services():
     services = response["serviceArns"]
     return services
 
-def delete_random_service():
+def delete_random_service(event, context):
     response = ecs.delete_service(
         cluster=cluster,
         service=random.choice(list_services())
     )
     print("chaos monkey randomly removed one of the services in " + json.dumps(response['service']['clusterArn']) + " and that service was a " + json.dumps(response['service']['serviceName'], indent=4, sort_keys=True, default=str))
-
-def handler(event, context):
-    delete_random_service()
